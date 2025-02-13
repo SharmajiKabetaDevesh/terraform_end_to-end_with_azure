7e6b491b351ce855f827c1380fcc882b33af23ad
SharmajiKabetaDevesh <deveshkumar.sharma@dmce.ac.in>
2025-02-13 13:19:51 +0530

    day-9 readme file added

--- Day-9/networks.tf
+++ Day-9/networks.tf
@@ -218,21 +218,3 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
   }
 }
 
-# create autoscale resource that will decrease the number of instances if the azurerm_orchestrated_scale set cpu usaae is below 10% for 2 minutes
-resource "azurerm_monitor_autoscale_setting" "autoscale" {
-  name                = "autoscale"
-  location            = azurerm_resource_group.resgroup.location
-  resource_group_name = azurerm_resource_group.resgroup.name
-  target_resource_id  = azurerm_linux_virtual_machine_scale_set.example.id
-  enabled             = true
-  profile {
-    name = "autoscale"
-    capacity {
-      default = 1
-      minimum = 1
-      maximum = 2
-    }
-
-    
-  }
-}
